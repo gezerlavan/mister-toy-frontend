5bd98ce1ba3bd772d742c9e392fde704f9e5aacd
Tal Elias <talelias2@gmail.com>
2023-09-23 11:20:44 +0300

    added homepage hero img. added toy imgs.

--- src/cmps/toy/ToyPreview.jsx
+++ src/cmps/toy/ToyPreview.jsx
@@ -4,8 +4,9 @@ export function ToyPreview({ toy }) {
     return (
         <Link to={`/toy/${toy._id}`}>
             <article className='toy-preview'>
-                <h1>Toy name: {toy.name}</h1>
-                <h1>Toy price: ${toy.price}</h1>
+                <h1 className="toy-name">{toy.name}</h1>
+                <img src={`https://robohash.org/${toy.name}?set=set1`} alt="" />
+                <h1>Price: ${toy.price}</h1>
                 <h1 className={toy.inStock ? 'green' : 'red'}>
                     {toy.inStock ? 'In stock' : 'Not in stock'}
                 </h1>
